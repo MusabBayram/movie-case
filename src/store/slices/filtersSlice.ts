d66283afcb5a8f9385fa4ece2195ec960e522335
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2024-12-03 13:42:22 +0300

    page number make 1 when user use filters

--- src/store/slices/filtersSlice.ts
+++ src/store/slices/filtersSlice.ts
@@ -22,15 +22,19 @@ const filtersSlice = createSlice({
     reducers: {
         setSearchTerm(state, action: PayloadAction<string>) {
             state.searchTerm = action.payload;
+            state.page = 1;
         },
         setSelectedTypes(state, action: PayloadAction<string[]>) {
             state.selectedTypes = action.payload;
+            state.page = 1;
         },
         setYear(state, action: PayloadAction<string>) {
             state.year = action.payload;
+            state.page = 1;
         },
         setMinRating(state, action: PayloadAction<number>) {
             state.minRating = action.payload;
+            state.page = 1;
         },
         setPage(state, action: PayloadAction<number>) {
             state.page = action.payload;
